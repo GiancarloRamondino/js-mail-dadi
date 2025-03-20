d9882bf776279db85cd454682d8641d929a986af
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-03-20 16:46:19 +0100

    [UPD]  Mail list update

--- script.js
+++ script.js
@@ -12,16 +12,15 @@ let mailFound = false;
 for (let i = 0; i > mailList.length; i++) {
     if (mailList[i] == mail) //controllo se la mail è presente forse avrei potuto usaree cheek?
         mailFound = true
-        
-    if (mailFound == true) { // ho usato le
-        console.log('la tua mail è già presente nella lista') //possso usare alert?
-    }
-    else {
-        console.log('la tua mail è stata registrata con successo') //alert?
-        let newMail = mailList.push(mail); //aggiungo la mia mail alla lista
-        console.log(mailList);
-    }
 }
 
+if (mailFound == true) { // ho usato le
+    console.log('la tua mail è già presente nella lista') //possso usare alert?
+}
+else {
+    console.log('la tua mail è stata registrata con successo') //alert?
+    mailList.push(mail); //aggiungo la mia mail alla lista
+    console.log(mailList);
+}
 
 
